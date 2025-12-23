a7930c5c9fc0151e6595e26ff1411c73ec640490
louai_abdi <104103186+Abdilouai@users.noreply.github.com>
2025-12-23 22:01:13 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,21 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+      // Exclude API files from client bundle
+      external: [/\/api\/.*/],
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 });
