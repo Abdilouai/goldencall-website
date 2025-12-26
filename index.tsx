61a64271b573e74bf6c4813dfaf69b11f30f3e19
louai_abdi <104103186+Abdilouai@users.noreply.github.com>
2025-12-26 18:04:13 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React, { useState, useEffect } from 'react';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { WhatsAppButton } from './components/WhatsAppButton';
+import { Home } from './pages/Home';
+import { Services } from './pages/Services';
+import { About } from './pages/About';
+import { Blog } from './pages/Blog';
+import { BookSession } from './pages/BookSession';
+import { BookingSuccess } from './pages/BookingSuccess';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+const App: React.FC = () => {
+  const [path, setPath] = useState(window.location.pathname);
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  useEffect(() => {
+    const handleLocationChange = () => {
+      setPath(window.location.pathname);
+    };
+
+    const originalPushState = window.history.pushState;
+    const originalReplaceState = window.history.replaceState;
+
+    window.history.pushState = function(...args: any[]) {
+      originalPushState.apply(this, args as any);
+      handleLocationChange();
+    };
+
+    window.history.replaceState = function(...args: any[]) {
+      originalReplaceState.apply(this, args as any);
+      handleLocationChange();
+    };
+
+    window.addEventListener('popstate', handleLocationChange);
+    
+    return () => {
+      window.history.pushState = originalPushState;
+      window.history.replaceState = originalReplaceState;
+      window.removeEventListener('popstate', handleLocationChange);
+    };
+  }, []);
+
+  const renderContent = () => {
+    switch (path) {
+      case '/':
+        return <Home />;
+      case '/services':
+        return <Services />;
+      case '/about':
+        return <About />;
+      case '/blog':
+        return <Blog />;
+      case '/book':
+        return <BookSession />;
+      case '/booking-success':
+        return <BookingSuccess />;
+      default:
+        return <Home />;
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-white font-sans text-dark flex flex-col">
+      <Navbar />
+      <main className="flex-grow">
+        {renderContent()}
+      </main>
+      <Footer />
+      <WhatsAppButton />
+      {/* Vercel Tracking Components */}
+      <Analytics />
+      <SpeedInsights />
+    </div>
+  );
+};
+
+export default App;
